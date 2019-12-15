4edd8485801c6cd8e43593ffbc0fbd8fef32ccf1
Anwesh43 <anweshthecool0@gmail.com>
2019-12-15 19:34:32 +0530

    adding Aimator class

---
+++ index.ts
@@ -0,0 +1,20 @@
+const delay = 100
+class Animator {
+
+    animated : boolean
+    interval : number
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
